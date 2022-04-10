e334d89f973d7d8ae5528413867681da9d51de8d
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-10 13:49:52 +0200

    end

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/*
-Consegna:
-Dati tre array contenenti:
- - una lista ordinata di 5 immagini,
- - una lista ordinata dei relativi 5 luoghi e
- - una lista di 5 news,
-creare un carosello come nella foto allegata.
-*/
 const images = [
   {
     image : "img/01.jpg",
@@ -34,37 +26,12 @@ const images = [
   },
 ]
 
-
-
-
-// const items = [
-//   "img/01.jpg",
-//   "img/02.jpg",
-//   "img/03.jpg",
-//   "img/04.jpg",
-//   "img/05.jpg"
-// ];
-
-// const title = ["Svezia", "Svizzera", "Gran Bretagna", "Germania", "Paradise"];
-
-// const text = [
-//   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
-//   "Lorem ipsum",
-//   "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-//   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
-//   "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
-// ];
-
-//variabile per raccogliere tutto l'html che va in items-container
 let itemTemplate = "";
 
-//variabile per raccogliere tutto l'html che va in thumbs-container
 let thumbTemplate = "";
 
-// preparo una varibile con l'indice dell'elemento attivo e la pongo inizialmente a 0 ovvero il primo elemento dell'array
 let currentIndexActive = 0;
 
-//eseguo il ciclo for sull'array delle immagini (items) e popolo l'html delle due varibaili da stampare nei due contenitori (immagini e thumbnails)
 for (let i = 0; i < images.length; i++) {
   let classActive = "";
   if (i === currentIndexActive) {
@@ -83,40 +50,29 @@ for (let i = 0; i < images.length; i++) {
     <img src="${images[i].image}" alt="" />
   </div>`;
 }
-//console.log(thumbTemplate);
 
-// metto in due variabili rispettivamente i contenitori che si trovano nell'html
 const itemsContainer = document.querySelector(".items-container");
 const thumbsContainer = document.querySelector(".thumbs-container");
-//console.log(itemContainer);
-
-//stampo l'html corrispondente nei due contenitori
 itemsContainer.innerHTML = itemTemplate;
 thumbsContainer.innerHTML += thumbTemplate;
-//document.querySelector(".item").classList.add("active");
-
-//Pulsanti
-//.next .fa-circle-chevron-down
-//.prev .fa-circle-chevron-up
-//metto nelle variabili next e prev i due pulsanti
 const next = document.querySelector(" .fa-circle-chevron-down");
 const prev = document.querySelector(" .fa-circle-chevron-up");
-//console.log(next, prev);
+const stop = document.querySelector(".fa-circle-stop")
+
 function slideDown() {
-  //prendere immagine con currentIndexActive e togliere classe active
   const imgs = document.getElementsByClassName("item");
   imgs[currentIndexActive].classList.remove("active");
   const thumbs = document.getElementsByClassName("thumb");
   thumbs[currentIndexActive].classList.remove("active");
-  //console.log(imgs);
+
   if (currentIndexActive === 4) {
     currentIndexActive = 0;
   } else {
     currentIndexActive++;
   }
-  //console.log(currentIndexActive);
+
   imgs[currentIndexActive].classList.add("active");
-  //console.log(currentIndexActive);
+
   thumbs[currentIndexActive].classList.add("active");
 }
 function slideUp() {
@@ -124,18 +80,20 @@ function slideUp() {
   imgs[currentIndexActive].classList.remove("active");
   const thumbs = document.getElementsByClassName("thumb");
   thumbs[currentIndexActive].classList.remove("active");
-  //console.log(imgs);
+
   if (currentIndexActive === 0) {
     currentIndexActive = images.length - 1;
   } else {
     currentIndexActive--;
   }
-  //console.log(currentIndexActive);
   imgs[currentIndexActive].classList.add("active");
-  //console.log(currentIndexActive);
   thumbs[currentIndexActive].classList.add("active");
 }
-
+function stopSlide(){
+  clearInterval(timer)
+}
 next.addEventListener("click", slideDown);
 prev.addEventListener("click", slideUp);
+stop.addEventListener("click", stopSlide )
 const timer = setInterval(slideDown, 4000) ;
+
